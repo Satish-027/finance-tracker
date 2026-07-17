71f1ff58e5712f5596439ddb5282c756ca18b179
Satish-027 <satishmendigeri123@gmail.com>
2026-07-17 10:33:58 +0530

    updated cinfig file

--- finance-tracker-backend/src/main/java/com/Project/finance_tracker/config/SecurityConfig.java
+++ finance-tracker-backend/src/main/java/com/Project/finance_tracker/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Project.finance_tracker.config;
 
 import com.Project.finance_tracker.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -28,6 +29,9 @@ public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
     private final UserDetailsService userDetailsService;
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -65,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
